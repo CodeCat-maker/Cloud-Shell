ab2167d8c918a1d8e96937501258d82f1670dd0c
Codecat <codecat@stud.tjut.edu.cn>
2021-08-23 15:28:44 +0800

    v0.2.0

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,8 @@
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const container = document.querySelector('.container');
-
+const btns = document.querySelectorAll('.btn');
+const loginBox = document.querySelector(".loginBox");
 left.addEventListener("mouseenter", () => {
     container.classList.add("hover-left");
 })
@@ -13,4 +14,14 @@ right.addEventListener("mouseenter", () => {
 })
 right.addEventListener("mouseleave", () => {
     container.classList.remove("hover-right");
+})
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        loginBox.style.display = "block";
+    })
+})
+document.querySelector(".LoginBtn").addEventListener("click", () => {
+    window.location.href = "/cloud";
+    alert("登陆成功");
+    return false;
 })
